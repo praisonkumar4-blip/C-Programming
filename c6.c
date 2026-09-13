30dac2df81ed6dde6b418a4f356cbc64362c167d
Gnana Praison .T <praisonkumar4@gmail.com>
2026-09-13 14:55:06 +0530

    Add files via upload

---
+++ c6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int a,b;
+    printf("Enter a Number 1 :");
+    scanf("%d",&a);
+    printf("Enter a number 2 :");
+    scanf("%d",&b);
+
+    if(a > b){
+        printf("A is Possitive");
+    }
+    else{
+        printf("B is Possitive");
+    }
+    
+    return 0;
+}
