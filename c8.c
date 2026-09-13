313c79382ce0a22ccece9dc1c68536bb6910970b
Gnana Praison .T <praisonkumar4@gmail.com>
2026-09-13 15:14:24 +0530

    Add files via upload

---
+++ c8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    
+    int num;
+    printf("Enter a number to check :");
+    scanf("%d",&num);
+    if(num%2==0){
+        printf("Given number is : EVEN");
+    }
+    else{
+        printf("Given number is : ODD");
+    }
+    return 0;
+}
